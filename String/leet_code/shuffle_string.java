09b8e7c87bb9e23513cb9eb0baf1285f1cee0c75
vignesh312002 <104010578+vignesh312002@users.noreply.github.com>
2024-05-26 14:53:33 +0530

    Add files via upload

---
+++ String/leet_code/shuffle_string.java
@@ -0,0 +1,22 @@
+//import java.util.Arrays;
+
+public class shuffle_string {
+    public static void main(String[] args) {
+        int []arr={4,5,6,7,0,2,1,3};
+        String s="codeleet";
+        System.out.println(restoreString(s,arr));
+            
+    }
+    public static String restoreString(String s, int[] indices) {
+        
+        char[] t =new char[s.length()];
+ 
+         for(int i=0;i<t.length;i++){
+             t[indices[i]]=s.charAt(i);
+         }
+         String str=new String(t);
+         return str ;
+     }
+    
+    
+}
